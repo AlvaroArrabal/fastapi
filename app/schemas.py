e9647291c66c5f11e61b3d8f7d39051d9706ddcb
AlvaroArrabal <arrabalalvaro@gmail.com>
2023-08-23 20:51:08 +0200

    relationship owner_id with user

--- app/schemas.py
+++ app/schemas.py
@@ -3,25 +3,6 @@ from datetime import datetime
 from typing import Optional
 
 
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
-class PostCreate(PostBase):
-    pass
-
-
-class Post(PostBase):
-    id: int
-    owner_id: int
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
-
-
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
@@ -39,6 +20,25 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+    owner: UserOut
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
 
 
 class Token(BaseModel):
