2a2f5dbac94b1d71dd1bc9860e4d50a072902bef
AlvaroArrabal <arrabalalvaro@gmail.com>
2023-08-26 19:53:46 +0200

    env variables -> security

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from .config import settings
 
 # "postgresql://<username>:<password>@<ip/address/hostname>/<database_name>"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345678@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
@@ -19,3 +22,17 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# Another way to connect to the db
+# while True:
+
+#     try:
+#         conn = psycopg2.connect(
+#             host='localhost', database='fastapi', user='postgres', password='12345678', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was succesfull!")
+#         break
+#     except Exception as error:
+#         print("Connection to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,33 +1,13 @@
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional, List
-import psycopg2
-from psycopg2.extras import RealDictCursor
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post, user, auth
+from .config import Settings
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-while True:
-
-    try:
-        conn = psycopg2.connect(
-            host='localhost', database='fastapi', user='postgres', password='12345678', cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connection to database failed")
-        print("Error: ", error)
-        time.sleep(2)
 
 
 app.include_router(post.router)
